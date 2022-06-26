707e213193fe900384d1e2987615e65549fb24a8
tuncAksuna <aksuna.tunc@gmail.com>
2022-06-26 15:38:00 +0300

    test class - crud operations tested

--- tuncode/src/test/java/com/interview/tuncode/TuncodeApplicationTests.java
+++ tuncode/src/test/java/com/interview/tuncode/TuncodeApplicationTests.java
@@ -1,13 +1,79 @@
 package com.interview.tuncode;
 
+import com.interview.tuncode.model.Student;
+import com.interview.tuncode.repository.StudentRepository;
+import com.interview.tuncode.test.TestCrud;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-class TuncodeApplicationTests {
+class TuncodeApplicationTests implements TestCrud {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    StudentRepository studentRepository;
 
+    @Test
+    @Override
+    public void testCreate() {
+        Student studentEntity = new Student();
+
+        studentEntity.setFirstName("Sylvie");
+        studentEntity.setLastName("Oksana");
+        studentEntity.setEmail("Oksana.Sylvie@gmail.com");
+        studentEntity.setCreatedTime("");
+        studentEntity.setUpdated(false);
+
+        studentRepository.save(studentEntity);
+
+        // If object is null send 'assertionError'
+        assertNotNull(studentRepository.findById(1L).orElse(null));
+    }
+
+    @Test
+    @Override
+    public void testList() {
+        List<Student> studentList = studentRepository.findAll();
+
+        // If object is greater than zero, there is object
+        assertThat(studentList).size().isGreaterThan(0);
+
+    }
+
+    @Test
+    @Override
+    public void testFindById() {
+        Student studentEntity = studentRepository.findById(1L).get();
+        assertEquals("Sylvie", studentEntity.getFirstName());
+    }
+
+    @Test
+    @Override
+    public void testUpdate() {
+        Student studentEntity = studentRepository.findById(1L).orElse(null);
+
+        assert studentEntity != null;
+        studentEntity.setFirstName("Sylvie 06");
+        studentEntity.setLastName("OKSANA");
+        studentEntity.setUpdated(true);
+        studentRepository.save(studentEntity);
+
+        // If objects are equals, send exception
+        assertNotEquals("Sylvie", Objects.requireNonNull(studentRepository.findById(1L).orElse(null)).getFirstName());
+    }
+
+    @Test
+    @Override
+    public void testDelete() {
+        studentRepository.deleteById(1L);
+
+        assertThat(studentRepository.existsById(1L)).isFalse();
+
+    }
 }
