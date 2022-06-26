629ddf3d3cb315b5365d5d5ebacbb0cdac3a1466
tuncAksuna <aksuna.tunc@gmail.com>
2022-06-26 15:35:36 +0300

    Test interface for test class

---
+++ tuncode/src/test/java/com/interview/tuncode/test/TestCrud.java
@@ -0,0 +1,14 @@
+package com.interview.tuncode.test;
+
+public interface TestCrud {
+
+    void testCreate();
+
+    void testUpdate();
+
+    void testList();
+
+    void testFindById();
+
+    void testDelete();
+}
