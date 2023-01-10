34a24afbb6ea7726b4998226efc0e5cfca41c5aa
caksuna <caksuna@innova.com.tr>
2023-01-10 21:50:29 +0300

    custom annotation for validation

---
+++ tuncode/src/main/java/com/interview/tuncode/configurations/annotations/BusinessClass.java
@@ -0,0 +1,13 @@
+package com.interview.tuncode.configurations.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BusinessClass {
+
+    Class value();
+}
