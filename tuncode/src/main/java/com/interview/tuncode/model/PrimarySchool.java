840a387776ae51aa3ba0336fdf8fb9438b00392c
caksuna <caksuna@innova.com.tr>
2023-06-18 14:22:19 +0300

    implemented jpa inheritance(discriminator)-2

---
+++ tuncode/src/main/java/com/interview/tuncode/model/PrimarySchool.java
@@ -0,0 +1,17 @@
+package com.interview.tuncode.model;
+
+import com.interview.tuncode.commonutils.CommonConstants;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@ToString
+
+@Entity
+@DiscriminatorValue(CommonConstants.PRIMARY_SCHOOL)
+public class PrimarySchool extends School {
+
+}
