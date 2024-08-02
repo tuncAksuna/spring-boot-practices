64bdeb92534352d82a3ad20fe05175a02c54fbd8
caksuna <caksuna@innova.com.tr>
2024-08-02 23:32:06 +0300

    some AOP methods created

--- tuncode/src/main/java/com/interview/tuncode/configurations/aspects/LoggingAspect.java
+++ tuncode/src/main/java/com/interview/tuncode/configurations/aspects/LoggingAspect.java
@@ -1,10 +1,16 @@
 package com.interview.tuncode.configurations.aspects;
 
+import com.interview.tuncode.configurations.response.AppResponse;
+import com.interview.tuncode.model.Address;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Aspect
 @Component
@@ -12,22 +18,33 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 
     /**
-     * TODO :  pointcutExpForServicesPackage ÇALIŞMIYOR
+     * It is triggered
+     * When all classes in all packages under the Controller package and all methods within the classes are called.
      */
-
     @Before("pointcutExpForControllerPackage()")
-    public void logBefore() {
-        log.info("testLogging method executed ! @Before controller");
-        System.out.println("testLogging method executed ! @Before controller");
+    public void logBefore(JoinPoint joinPoint) {
+        log.info("{} ! \t Method name: {} \t Declaring type: {}",
+                joinPoint.getKind().toUpperCase(),
+                joinPoint.getSignature().getName(),
+                joinPoint.getSignature().getDeclaringTypeName());
+    }
+
+    /**
+     * Triggered after "getAllAddresses" method runs successfully
+     */
+    @AfterReturning(pointcut = "execution(* com.interview.tuncode.controllers.address.AdressController.getAllAdresses(..))",
+            returning = "response")
+    public void afterReturningCreateStudent(JoinPoint joinPoint, AppResponse<List<Address>> response) {
+        log.info("{} ! \t Method name: {}  \t Response: {} \t Status Code: {}",
+                joinPoint.getKind().toUpperCase(),
+                joinPoint.getSignature().getName(),
+                response.getData(),
+                response.getStatus());
     }
 
     @Pointcut("execution(* com.interview.tuncode.controllers..*(..))")
     private void pointcutExpForControllerPackage() {
 
-    }
-
-    @Pointcut("execution(* com.interview.tuncode.controllers.student.StudentController.testLoggingAspect(..))")
-    private void pointcutExpForTestLoggingAspectMethod() {
     }
 
     @Pointcut("execution(* com.interview.tuncode.services.student.StudentServiceImp.*(..))")
