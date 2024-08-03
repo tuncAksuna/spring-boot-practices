113aa14448ecfc04647c2dada569f12902b76c92
caksuna <caksuna@innova.com.tr>
2024-08-03 15:57:23 +0300

    @Around AOP for updateStudent Method

--- tuncode/src/main/java/com/interview/tuncode/configurations/aspects/LoggingAspect.java
+++ tuncode/src/main/java/com/interview/tuncode/configurations/aspects/LoggingAspect.java
@@ -1,10 +1,14 @@
 package com.interview.tuncode.configurations.aspects;
 
 import com.interview.tuncode.configurations.response.AppResponse;
+import com.interview.tuncode.exceptions.SourceAlreadyExistsException;
+import com.interview.tuncode.exceptions.SourceNotFoundException;
 import com.interview.tuncode.model.Address;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -50,6 +54,27 @@ public class LoggingAspect {
                 joinPoint.getSignature().getDeclaringTypeName(),
                 exc.toString());
     }
+
+    @Around("execution(public void updateStudent(..))")
+    public Object aroundForAllServicesPackage(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long startTime = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long endTime = System.currentTimeMillis();
+            log.info("\n {} ! \n Duration milliseconds: {} \n RESULT: {} ",
+                    proceedingJoinPoint.getKind().toUpperCase(),
+                    endTime - startTime / 1000.0,
+                    result);
+
+        } catch (Exception exc) {
+            log.error("@Around Exception: {}", exc.toString());
+            throw new SourceNotFoundException(exc.getMessage());
+        }
+
+        return result;
+    }
+
 
     @Pointcut("execution(* com.interview.tuncode.controllers..*(..))")
     private void pointcutExpForControllerPackage() {
